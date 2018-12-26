e65ff33d6f87411b1f23620bc5bdfe80a0a80972
Luca Pradovera <luca.pradovera@gmail.com>
2018-12-26 18:07:57 +0100

    Full stack tests

--- spec/signalwire/sdk/voice_response_spec.rb
+++ spec/signalwire/sdk/voice_response_spec.rb
@@ -8,13 +8,13 @@ module Signalwire
       response = Signalwire::Sdk::VoiceResponse.new do |r|
         r.say(message: 'hello there', voice: 'alice')
         r.dial(caller_id: '+14159992222') do |d|
-          d.client 'jenny'
+          d.number('+15552233444')
         end
       end
 
       expect(response.to_s).to eq "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n"\
         "<Say voice=\"alice\">hello there</Say>\n<Dial callerId=\"+14159992222\">\n"\
-        "<Client>jenny</Client>\n</Dial>\n</Response>\n"
+        "<Number>+15552233444</Number>\n</Dial>\n</Response>\n"
     end
   end
 end
