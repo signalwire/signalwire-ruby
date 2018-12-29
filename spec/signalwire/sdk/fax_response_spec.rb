4f7b354c61ae92836a4b08cf12fa3f9bddeb9e76
Luca Pradovera <luca.pradovera@gmail.com>
2018-12-29 11:59:59 +0100

    Add fax support

---
+++ spec/signalwire/sdk/fax_response_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+module Signalwire
+  RSpec.describe Sdk::FaxResponse do
+    it 'generates the correct LAML' do
+      response = Signalwire::Sdk::FaxResponse.new do |r|
+        r.receive(action: '/receive/fax')
+      end
+
+      expect(response.to_s).to eq "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n"\
+        "<Receive action=\"/receive/fax\"/>\n</Response>\n"
+    end
+
+    it 'can reject a fax' do
+      response = Signalwire::Sdk::FaxResponse.new do |r|
+        r.reject
+      end
+
+      expect(response.to_s).to eq "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n"\
+        "<Reject/>\n</Response>\n"
+    end
+  end
+end
+
