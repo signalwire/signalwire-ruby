0b20d8ea912db0387577ded94cee639150f2fd9e
Luca Pradovera <luca.pradovera@gmail.com>
2019-03-06 17:26:34 +0100

    Pass in domain via parameter

--- lib/signalwire/rest/client.rb
+++ lib/signalwire/rest/client.rb
@@ -2,5 +2,14 @@
 
 module Signalwire::REST
   class Client < Twilio::REST::Client
+    def initialize(account, token, space = nil)
+      unless space.nil?
+        Signalwire::Sdk.configure do |config|
+          config.hostname = space
+        end
+      end
+
+      super(account, token)
+    end
   end
 end
