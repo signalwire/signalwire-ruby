b0cf077384ef9d9576dccfab1d231a55b344ef22
Bryan Rite <bryan@signalwire.com>
2019-07-19 11:43:18 -0700

    Update fax send example with keyword arg.

--- examples/relay/fax/fax_send.rb
+++ examples/relay/fax/fax_send.rb
@@ -12,7 +12,7 @@ class OutboundConsumer < Signalwire::Relay::Consumer
   def ready
     call = client.calling.new_call(from: ENV['FAX_FROM_NUMBER'], to: ENV['FAX_TO_NUMBER'])
     call.dial
-    call.fax_send("https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
+    call.fax_send(document: "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf")
   rescue StandardError => e
     logger.error e.inspect
     logger.error e.backtrace
