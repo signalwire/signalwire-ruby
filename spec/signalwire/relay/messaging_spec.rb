f50fb91cedf49d3a7535b78fb4ebfc84f5490198
Luca Pradovera <luca.pradovera@gmail.com>
2019-08-02 13:17:47 +0200

    Fix renamed parameters

--- spec/signalwire/relay/messaging_spec.rb
+++ spec/signalwire/relay/messaging_spec.rb
@@ -49,4 +49,20 @@ describe Signalwire::Relay::Messaging::Instance do
       expect(received.state).to eq "received"
     end
   end
+
+  describe "#send" do
+    it "sends the proper payload" do
+      expect(subject).to receive(:relay_execute).with({
+        method: 'messaging.send',
+        protocol: subject.protocol,
+        params: {
+          body: 'Hello from SignalWire!',
+          context: 'outbound',
+          from_number: '+1555XXXXXXX',
+          to_number: '+1555XXXXXYY'
+        }
+      })
+      subject.messaging.send(from: "+1555XXXXXXX", to: "+1555XXXXXYY", body: "Hello from SignalWire!", context: 'outbound')
+    end
+  end
 end
