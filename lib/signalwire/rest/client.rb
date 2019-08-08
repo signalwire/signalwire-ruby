d1060793beb5174f3e0370dc215a126233f1e897
Luca Pradovera <luca.pradovera@gmail.com>
2019-08-08 14:00:16 -0500

    Restore the correct parameter for the REST client space URL

--- lib/signalwire/rest/client.rb
+++ lib/signalwire/rest/client.rb
@@ -3,7 +3,7 @@
 module Signalwire::REST
   class Client < Twilio::REST::Client
     def initialize(username = nil, password = nil, account_sid = nil, region = nil, http_client = Twilio::HTTP::Client.new, **args)
-      host = args.delete(:SIGNALWIRE_HOST)
+      host = args.delete(:signalwire_space_url)
 
       unless host.nil?
         Signalwire::Sdk.configure do |config|
