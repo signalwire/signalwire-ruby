84b8f54b9f7d8bd4d79811ec6a7b551b4ab82eb4
Luca Pradovera <luca.pradovera@gmail.com>
2019-08-20 18:24:14 +0200

    Allow keyword and positional parameters in older methods

--- lib/signalwire/relay/calling/call_convenience_methods.rb
+++ lib/signalwire/relay/calling/call_convenience_methods.rb
@@ -16,35 +16,49 @@ module Signalwire::Relay::Calling
       play! silence_payload(duration)
     end
 
-    def play_tts(sentence, language='en-US', gender='female')
+    def play_tts(sentence_p=nil, language_p=Relay::DEFAULT_LANGUAGE, gender_p=Relay::DEFAULT_GENDER, sentence: nil, language: Relay::DEFAULT_LANGUAGE, gender: Relay::DEFAULT_GENDER)
+      set_parameters(binding, %i{sentence language gender}, %i{sentence})
+
       play tts_payload(sentence, language, gender)
     end
 
-    def play_tts!(sentence, language='en-US', gender='female')
+    def play_tts!(sentence_p=nil, language_p=Relay::DEFAULT_LANGUAGE, gender_p=Relay::DEFAULT_GENDER, sentence: nil, language: Relay::DEFAULT_LANGUAGE, gender: Relay::DEFAULT_GENDER)
+      set_parameters(binding, %i{sentence language gender}, %i{sentence})
       play! tts_payload(sentence, language, gender)
     end
 
-    def prompt_audio(collect, url)
+    def prompt_audio(collect_p = nil, url_p = nil, collect: nil, url: nil)
+      set_parameters(binding, %i{collect url}, %i{collect url})
+
       prompt(collect, audio_payload(url))
     end
 
-    def prompt_audio!(collect, url)
+    def prompt_audio!(collect_p = nil, url_p = nil, collect: nil, url: nil)
+      set_parameters(binding, %i{collect url}, %i{collect url})
+
       prompt!(collect, audio_payload(url))
     end
 
-    def prompt_silence(collect, duration)
+    def prompt_silence(collect_p = nil, duration_p = nil, collect: nil, duration: nil)
+      set_parameters(binding, %i{collect duration}, %i{collect duration})
+
       prompt(collect, silence_payload(duration))
     end
 
-    def prompt_silence!(collect, url)
+    def prompt_silence!(collect_p = nil, duration_p = nil, collect: nil, duration: nil)
+      set_parameters(binding, %i{collect duration}, %i{collect duration})
+
       prompt!(collect, silence_payload(duration))
     end
 
-    def prompt_tts(collect, sentence, language='en-US', gender='female')
+    def prompt_tts(collect_p = nil, sentence_p = nil, language_p=Relay::DEFAULT_LANGUAGE, gender_p=Relay::DEFAULT_GENDER, collect: nil, sentence: nil, language: Relay::DEFAULT_LANGUAGE, gender: Relay::DEFAULT_GENDER)
+      set_parameters(binding, %i{collect sentence language gender}, %i{collect sentence})
       prompt(collect, tts_payload(sentence, language, gender))
     end
 
-    def prompt_tts!(collect, sentence, language='en-US', gender='female')
+    def prompt_tts!(collect_p = nil, sentence_p = nil, language_p=Relay::DEFAULT_LANGUAGE, gender_p=Relay::DEFAULT_GENDER, collect: nil, sentence: nil, language: Relay::DEFAULT_LANGUAGE, gender: Relay::DEFAULT_GENDER)
+      set_parameters(binding, %i{collect sentence language gender}, %i{collect sentence})
+
       prompt!(collect, tts_payload(sentence, language, gender))
     end
 
@@ -64,6 +78,8 @@ module Signalwire::Relay::Calling
       wait_for(Relay::CallState::ENDED)
     end
 
+    private 
+
     def audio_payload(url)
       [{ type: "audio", params: { url: url } }]
     end
@@ -72,7 +88,7 @@ module Signalwire::Relay::Calling
       [{ type: "silence", params: { duration: duration } }]
     end
 
-    def tts_payload(sentence, language='en-US', gender='male')
+    def tts_payload(sentence, language=Relay::DEFAULT_LANGUAGE, gender=Relay::DEFAULT_GENDER)
       [{ "type": 'tts', "params": { "text": sentence, "language": language, "gender": gender } }]
     end
   end
