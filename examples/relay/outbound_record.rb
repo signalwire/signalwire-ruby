bc0f74973d80a06eb29477c1b5bc1ea139f12e7f
Luca Pradovera <luca.pradovera@gmail.com>
2019-08-20 18:36:12 +0200

    Changelog and examples

--- examples/relay/outbound_record.rb
+++ examples/relay/outbound_record.rb
@@ -14,9 +14,9 @@ class OutboundConsumer < Signalwire::Relay::Consumer
     logger.info 'Dialing out'
     call = client.calling.new_call(from: ENV['FROM_NUMBER'], to: ENV['TO_NUMBER'])
     call.dial
-    call.play_tts 'please leave your message after the beep. Press pound when done.'
+    call.play_tts sentence: 'please leave your message after the beep. Press pound when done.'
     result = call.record({"audio": { "beep": "true", "terminators": "#"}})
-    call.play_tts 'you said:'
+    call.play_tts sentence: 'you said:'
     call.play_audio result.url
     call.hangup
   end
--- examples/relay/wait_for_ending.rb
+++ examples/relay/wait_for_ending.rb
@@ -12,14 +12,14 @@ class MyConsumer < Signalwire::Relay::Consumer
   contexts ['incoming']
   def on_incoming_call(call)
     call.answer
-    call.play_tts 'Welcome to Relay'
+    call.play_tts sentence: 'Welcome to Relay'
     dial = call.connect [[{ type: 'phone', params: { to_number: MY_NUMBER, from_number: call.from, timeout: 30 } }]]
     pp "Connected"
     pp dial.successful
-    dial.call.play_tts "Hello!" if dial.successful
+    dial.call.play_tts(sentence: "Hello!") if dial.successful
     pp "Waiting on Ending"
     dial.call.wait_for_ending
-    call.play_tts 'finished. hanging up.'
+    call.play_tts sentence: 'finished. hanging up.'
     call.hangup
   rescue StandardError => e
     logger.error e.inspect
